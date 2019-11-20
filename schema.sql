cc4ca9688c5f967bca81a47f676047465dc646e1
diksha nagrale <dikshanagrale18@gmail.com>
2019-11-20 15:40:14 +0530

    initial db

---
+++ schema.sql
@@ -0,0 +1,6 @@
+
+create table emp(no int,name varchar(50), address varchar(50));
+insert into emp values(1,"diksha","nagpur");
+insert into emp values(2,"diksha1","nagpur1");
+insert into emp values(3,"diksha2","nagpur2");
+
